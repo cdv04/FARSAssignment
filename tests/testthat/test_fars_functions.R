ffc422116913179497cdb0095972f1156b5593b8
David W. Body <davidbody@bigcreek.com>
2016-12-22 10:36:41 -0600

    Test for make_filename

--- tests/testthat/test_fars_functions.R
+++ tests/testthat/test_fars_functions.R
@@ -10,3 +10,7 @@ test_that("fars_read returns a data frame", {
 test_that("fars_read generates an error if the file doesn't exist", {
     expect_error(fars_read("bogus"), "file 'bogus' does not exist")
 })
+
+test_that("make_filename returns a proper filename", {
+    expect_match(make_filename(2015), "accident_2015.csv.bz2")
+})
